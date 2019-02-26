451980f3bea7b9d5d512974ed229f3e4169469d5
Cadel Watson <cadel.watson@agridigital.io>
2019-02-26 19:41:47 +1100

    Use 4 spaces instead of 2

--- src/index.js
+++ src/index.js
@@ -6,59 +6,59 @@ const Path = require("path");
 const Nunjucks = require("nunjucks");
 
 const server = Hapi.server({
-  host: "localhost",
-  port: 3000
+    host: "localhost",
+    port: 3000
 });
 
 const context = {
-  title: "Prices"
+    title: "Prices"
 };
 
 const start = async () => {
-  await server.register(Vision);
+    await server.register(Vision);
 
-  server.views({
-    engines: {
-      html: {
-        // Nunjucks setup from https://github.com/hapijs/vision README
-        compile: (src, options) => {
-          const template = Nunjucks.compile(src, options.environment);
+    server.views({
+        engines: {
+            html: {
+                // Nunjucks setup from https://github.com/hapijs/vision README
+                compile: (src, options) => {
+                    const template = Nunjucks.compile(src, options.environment);
 
-          return context => template.render(context);
-        },
-        prepare: (options, next) => {
-          options.compileOptions.environment = Nunjucks.configure(
-            options.path,
-            {
-              watch: false
+                    return context => template.render(context);
+                },
+                prepare: (options, next) => {
+                    options.compileOptions.environment = Nunjucks.configure(
+                        options.path,
+                        {
+                            watch: false
+                        }
+                    );
+
+                    return next();
+                }
             }
-          );
+        },
+        path: Path.join(__dirname, "templates"),
+        context
+    });
 
-          return next();
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: {
+            view: "index"
         }
-      }
-    },
-    path: Path.join(__dirname, "templates"),
-    context
-  });
+    });
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: {
-      view: "index"
+    try {
+        await server.start();
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
     }
-  });
 
-  try {
-    await server.start();
-  } catch (e) {
-    console.log(e);
-    process.exit(1);
-  }
-
-  console.log("Server online at", server.info.uri);
-  console.log(__dirname);
+    console.log("Server online at", server.info.uri);
+    console.log(__dirname);
 };
 
 start();
