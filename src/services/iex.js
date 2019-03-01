3caa97aa36c7648cd8355d4c209a82fbdf78ab31
Cadel Watson <cadel.watson@agridigital.io>
2019-03-02 10:21:54 +1100

    Delete commented code

--- src/services/iex.js
+++ src/services/iex.js
@@ -1,25 +1,5 @@
 const fetch = require("node-fetch");
 
-// function IexService() {
-//     this.api = "https://api.iextrading.com/1.0";
-//
-//     this.get = endpoint => {
-//         return fetch(endpoint).then(res => res.json());
-//     };
-//
-//     this.getCompany = async symbol => {
-//         return this.get(`${this.api}/stock/${symbol}/company`);
-//     };
-// }
-
-// IexService.prototype.get = endpoint => {
-//     return fetch(endpoint).then(res => res.json());
-// };
-//
-// IexService.prototype.getCompany = async symbol => {
-//     return this.get(`${this.api}/stock/${symbol}/company`);
-// };
-
 class IexService {
     constructor() {
         this.api = "https://api.iextrading.com/1.0";
